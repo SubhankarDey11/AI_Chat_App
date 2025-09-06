9b78bfd02253738d54ea81193e47027f8c820c5f
Subhankar <deysubhankar436@gmail.com>
2025-09-06 15:09:40 +0530

    completed

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import chatbotRoutes from './routes/chatbot.route.js';
+
+const app = express()
+dotenv.config();
+
+
+const port =process.env.PORT || 3000
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+//Database connection code 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("connected to mongodb")
+}).catch((error) => {
+    console.log("error connecting to mongodb:", error)
+})
+
+
+//Defining Routes
+app.use('/bot/v1/', chatbotRoutes)
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+
